fe6240da9b8b6fc1727011e6b77184ead87504c0
theertha_avnsh <theerthaavnsh@gmail.com>
2023-05-15 14:30:44 +0530

    update

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -31,7 +31,7 @@ router.route('/').post(async (req, res) => {
 
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.error(error);
+   console.error("There was an error");
     res.status(500).json({ message: "Something went wrong" })
   }
 })
